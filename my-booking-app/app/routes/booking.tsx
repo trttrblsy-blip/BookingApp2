6514bf3151c1f4410dceb3bbea1e3df66cc68066
daniel <danielorshnn@gmail.com>
2026-07-08 11:13:09 +0300

    initial

---
+++ my-booking-app/app/routes/booking.tsx
@@ -0,0 +1,22 @@
+import {data,Route} from 'react-router'
+import { PrismaClient } from "../../prisma/client";
+import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+import { fetch as undiciFetch } from "undici"; // Only for Node.js <18
+
+const adapter = new PrismaPlanetScale({
+  url: process.env.DATABASE_URL,
+  fetch: undiciFetch,
+});
+const prisma = new PrismaClient({ adapter });
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const { date } = params;
+  const booking=await prisma.booking.findFirst({
+    where: {
+     AND:{startDate:{gte:date},endDate:{lte:date}}}
+    },
+  );
+  if (!booking) throw data(null, { status: 404 });
+
+  return booking;
+}
